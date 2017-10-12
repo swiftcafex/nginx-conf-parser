1f2dad71e3269c779faa281264f2256f18f9837a
SwiftCafe <swiftcafex@gmail.com>
2017-10-12 23:52:13 +0800

    1
    add unit test for ConfParser.extractLines

--- test/test-conf-parser.js
+++ test/test-conf-parser.js
@@ -10,10 +10,20 @@ test('test', t => {
 test('extract lines', async t => {
 	const exmaplePath = './test/support/example.conf';
 	const inStream = fs.createReadStream(exmaplePath);
-	await confParser.extractLines(inStream, lines => {
-		console.log(lines.count);
-		t.pass();
-	});
 
+	await (function () {
+		return new Promise(resolve => {
+			confParser.extractLines(inStream, lines => {
+				t.is(lines.length, 70);	// The number of lines must be 70, same as the oroginal file /test/support/example.conf
+
+				// Comments must be stripped after extract.
+				for (const line of lines) {
+					t.is(line.indexOf('#'), -1);
+				}
+
+				resolve();
+			});
+		});
+	})();
 	t.pass();
 });
